af01e9c49bf54c04cbd6084a6e8030fadc1d244b
KANUFRE <william.kanufre>
2024-06-24 09:23:55 -0300

    Alterado dia 24/06/2024

--- js/contact.js
+++ js/contact.js
@@ -1,102 +1,102 @@
 class FormSubmit {
-  constructor(settings) {
-      this.settings = settings;
-      this.form = document.querySelector(settings.form);
-      this.formButton = document.querySelector(settings.button);
-      if (this.form) {
-          this.url = this.form.getAttribute("action");
-      }
-      this.sendForm = this.sendForm.bind(this);
+    constructor(settings) {
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+        this.sendForm = this.sendForm.bind(this);
+    }
+  
+    displaySuccess() {
+        this.form.innerHTML = this.settings.success;
+        setTimeout(() => {
+            window.location.reload();
+        }, 3000); // Recarregar a página após 2 segundos
+    }
+  
+    displayError() {
+        this.form.innerHTML = this.settings.error;
+    }
+  
+    getFormObject() {
+        const formObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+        fields.forEach((field) => {
+            formObject[field.getAttribute("name")] = field.value;
+        });
+        return formObject;
+    }
+  
+    validateEmail(email) {
+      //   const allowedDomains = ["gmail.com","gmail.com.br", "outlook.com.br","outlook.com", "hotmail.com","hotmail.com.br"];
+      //   const emailDomain = email.split("@")[1];
+      //   email.indexOf('.com') > -1 || email.indexOf('.com.br') > -1
+      //   return allowedDomains.includes(emailDomain);
+        return email.indexOf('.com') > -1 || email.indexOf('.com.br') > -1
+    }
+  
+    validateForm() {
+        const nome = this.form.querySelector("#nome").value.trim();
+        const email = this.form.querySelector("#email").value.trim();
+        const mensagem = this.form.querySelector("#mensagem").value.trim();
+  
+        if (!nome || !email || !mensagem) {
+            alert("Todos os campos são obrigatórios.");
+            return false;
+        }
+  
+        if (!this.validateEmail(email)) {
+            alert("Por favor, utilize um email válido.");
+            return false;
+        }
+  
+        return true;
+    }
+  
+    onSubmission(event) {
+        event.preventDefault();
+        if (!this.validateForm()) {
+            this.formButton.disabled = false; // Re-enable button if validation fails
+            return;
+        }
+        this.formButton.disabled = true;
+        this.formButton.innerText = "Enviando...";
+    }
+  
+    async sendForm(event) {
+        try {
+            this.onSubmission(event);
+            if (this.formButton.disabled) { // Only proceed if button is disabled
+                await fetch(this.url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                    body: JSON.stringify(this.getFormObject()),
+                });
+                this.displaySuccess();
+            }
+        } catch (error) {
+            this.displayError();
+            this.formButton.disabled = false; // Re-enable button if there is an error
+            this.formButton.innerText = "Enviar";
+            throw new Error(error);
+        }
+    }
+  
+    init() {
+        if (this.form) this.formButton.addEventListener("click", this.sendForm);
+        return this;
+    }
   }
-
-  displaySuccess() {
-      this.form.innerHTML = this.settings.success;
-      setTimeout(() => {
-          window.location.reload();
-      }, 3000); // Recarregar a página após 2 segundos
-  }
-
-  displayError() {
-      this.form.innerHTML = this.settings.error;
-  }
-
-  getFormObject() {
-      const formObject = {};
-      const fields = this.form.querySelectorAll("[name]");
-      fields.forEach((field) => {
-          formObject[field.getAttribute("name")] = field.value;
-      });
-      return formObject;
-  }
-
-  validateEmail(email) {
-    //   const allowedDomains = ["gmail.com","gmail.com.br", "outlook.com.br","outlook.com", "hotmail.com","hotmail.com.br"];
-    //   const emailDomain = email.split("@")[1];
-    //   email.indexOf('.com') > -1 || email.indexOf('.com.br') > -1
-    //   return allowedDomains.includes(emailDomain);
-      return email.indexOf('.com') > -1 || email.indexOf('.com.br') > -1
-  }
-
-  validateForm() {
-      const nome = this.form.querySelector("#nome").value.trim();
-      const email = this.form.querySelector("#email").value.trim();
-      const mensagem = this.form.querySelector("#mensagem").value.trim();
-
-      if (!nome || !email || !mensagem) {
-          alert("Todos os campos são obrigatórios.");
-          return false;
-      }
-
-      if (!this.validateEmail(email)) {
-          alert("Por favor, utilize um email válido.");
-          return false;
-      }
-
-      return true;
-  }
-
-  onSubmission(event) {
-      event.preventDefault();
-      if (!this.validateForm()) {
-          this.formButton.disabled = false; // Re-enable button if validation fails
-          return;
-      }
-      this.formButton.disabled = true;
-      this.formButton.innerText = "Enviando...";
-  }
-
-  async sendForm(event) {
-      try {
-          this.onSubmission(event);
-          if (this.formButton.disabled) { // Only proceed if button is disabled
-              await fetch(this.url, {
-                  method: "POST",
-                  headers: {
-                      "Content-Type": "application/json",
-                      Accept: "application/json",
-                  },
-                  body: JSON.stringify(this.getFormObject()),
-              });
-              this.displaySuccess();
-          }
-      } catch (error) {
-          this.displayError();
-          this.formButton.disabled = false; // Re-enable button if there is an error
-          this.formButton.innerText = "Enviar";
-          throw new Error(error);
-      }
-  }
-
-  init() {
-      if (this.form) this.formButton.addEventListener("click", this.sendForm);
-      return this;
-  }
-}
-
-const formSubmit = new FormSubmit({
-  form: "[data-form]",
-  button: "[data-button]",
-  success: "<h1 class='success'>Mensagem enviada!</h1>",
-  error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
-});
-formSubmit.init();
+  
+  const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+  });
+  formSubmit.init();
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,12 @@
+
 document.getElementById('btn-menu').onclick = function() {
     const menuMobile = document.getElementById('menu-mobile');
-    menuMobile.classList.toggle('active');
+        menuMobile.classList.toggle('active');
 };
 
 document.querySelectorAll('#menu-mobile nav a').forEach(link => {
     link.onclick = function() {
         const menuMobile = document.getElementById('menu-mobile');
-        menuMobile.classList.remove('active');
-    };
+            menuMobile.classList.remove('active');
+        };
 });
